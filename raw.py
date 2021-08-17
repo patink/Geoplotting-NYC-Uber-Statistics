e6d76b7a30a29ae48dc2d07bc2a6f7d8c127b4aa
Nisith Pati <54003224+patink@users.noreply.github.com>
2021-08-18 01:19:23 +0530

    Add files via upload

---
+++ raw.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+DATE_TIME = 'date/time'
+
+@st.cache
+def load_data(nrows):
+    data=pd.read_csv("uber-raw-data-sep14.csv", nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase,axis='columns', inplace=True)
+    data[DATE_TIME] = pd.to_datetime(data[DATE_TIME])
+    return data
+
+data = load_data(10000)
+
+hour = st.slider('Selected hour',0,0,23,1)
+data = data[data[DATE_TIME].dt.hour == hour]
+ 
+if st.checkbox('View Data'):
+    st.subheader('Raw Data')
+    st.write(data)
+
+st.subheader('Data by Minute at %sh' % hour)    
+st.bar_chart(np.histogram(data[DATE_TIME].dt.minute, bins=60, range=(0,60))[0])
+
+st.subheader('Geodata at %sh' % hour)
+st.map(data)
